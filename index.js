29ac1b9cf44268db63953de9f808196394063d85
SergioLM <sergiolillom@gmail.com>
2024-06-25 16:11:58 +0200

    home.pug con el form básico

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
 
+const apiKey = process.env.API_KEY;
+
+//Cada dato que reciba la API, lo convierte a JSON
+app.use(express.json()); // Habilito recepción de JSON en servidor
+app.use(express.urlencoded({extended: true})); // Habilito recepción de formularios en servidor
+
+//Configuración de vistas con PUG (motor de plantillas) No hace falta un require; el sistema ya sabe que esta instalado
+///Directamente va a buscarlas en ./views
+app.set('view engine', 'pug');
+app.set('views','./views');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('home.pug')
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Listening on port 3000!')
 })
